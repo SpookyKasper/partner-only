b4c955f1b49836336766248c4a1fcf105896ce45
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-02-23 11:19:31 +0100

    Add some posts views and controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,34 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user, only: [:new, :create, :edit]
+
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save?
+      flash[:notice] = 'Post created ;)'
+      redirect_to @post
+    else
+      flash[:alert] = 'Problem while creating the post :/'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
